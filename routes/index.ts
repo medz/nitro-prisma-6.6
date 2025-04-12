2de9e8bb53dfb7aa327d5d69542e017bda6fdec1
Seven Du <5564821+medz@users.noreply.github.com>
2025-04-12 10:41:37 +0800

    Update index.ts

--- routes/index.ts
+++ routes/index.ts
@@ -2,6 +2,6 @@ import { PrismaD1 } from "@prisma/adapter-d1";
 import { PrismaClient } from "~/prisma/client";
 
 export default defineEventHandler((event) => {
-  const adapter = new PrismaD1(event.context.cloudflare.env.DB_MBTI as any);
+  const adapter = new PrismaD1(event.context.cloudflare.env.DB as any);
   const client = new PrismaClient({ adapter });
 });
